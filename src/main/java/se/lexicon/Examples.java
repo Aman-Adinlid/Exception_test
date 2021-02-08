2c595fd64dddbfbd3159ec0a5831c161e29b58ef
aman adinlid <95pinkpanda@gmail.com>
2021-02-08 17:41:35 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/Examples.java
@@ -0,0 +1,46 @@
+package se.lexicon;
+
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Examples {
+
+    public static void main(String[] args) {
+        List<String> content = Arrays.asList("Hello", "World");
+        File url = new File(File.pathSeparator);
+        write(url, content);
+
+        for (String string : read(url)) {
+            System.out.println(string);
+        }
+    }
+
+    public static List<String> write(File destination, List<String> src){
+        try(BufferedWriter writer = new BufferedWriter((new FileWriter(destination)))){
+            for (String toWrite : src){
+                writer.write(toWrite);
+                writer.newLine();
+            }
+            writer.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return src;
+    }
+
+    public static List<String> read(File source) {
+        List<String> strings = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                strings.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return strings;
+    }
+}
